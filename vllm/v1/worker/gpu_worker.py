f239ace7812377bf71e9cb298ecd4e1a4bba5196
Dan Aloni <dan.aloni@vastdata.com>
2025-06-07 10:32:14 +0300

    Allow to override KV cache memory calculation hacks
    Signed-off-by: Dan Aloni <dan.aloni@vastdata.com>

--- vllm/v1/worker/gpu_worker.py
+++ vllm/v1/worker/gpu_worker.py
@@ -212,7 +212,19 @@ class Worker(WorkerBase):
             total_gpu_memory * self.cache_config.gpu_memory_utilization -
             peak_memory)
 
-        return int(available_kv_cache_memory)
+        available_kv_cache_memory = int(available_kv_cache_memory)
+        logger.info(
+            "Determined amount of memory available to KV cache: %d bytes",
+            available_kv_cache_memory)
+
+        vllm_avail_kv_cache_override = os.getenv("VLLM_AVAIL_KV_CACHE_OVERRIDE", None)
+        if vllm_avail_kv_cache_override:
+            available_kv_cache_memory = int(vllm_avail_kv_cache_override)
+            logger.warning(
+                "Overriding amount of memory available to KV cache: %d bytes",
+                available_kv_cache_memory)
+
+        return available_kv_cache_memory
 
     def get_kv_cache_spec(self) -> dict[str, KVCacheSpec]:
         return self.model_runner.get_kv_cache_spec()
